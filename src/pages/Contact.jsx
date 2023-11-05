ed8a1881aacb2875569f95e5b93b38e0c04f96b7
Tebbo <leetebbs@ymail.com>
2023-11-05 19:47:24 +0000

    Create Contact us form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/contact.css";
 
-const Contact = () => {
-  return <div>Contact</div>;
-};
+function Contact() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission logic here, such as sending the data to a server
+    console.log("Form submitted:", formData);
+    // You can add API calls or other logic to handle the form data
+  };
+
+  return (
+    <section>
+      <div className="container">
+        <h1>Contact Us</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="message">Message:</label>
+            <textarea
+              id="message"
+              name="message"
+              value={formData.message}
+              onChange={handleInputChange}
+              required
+            ></textarea>
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </section>
+  );
+}
 
 export default Contact;
